1a5b74eb3a5c54468b12bc7761537ec68c6f5c2b
Fabryprog <fabryprog@gmail.com>
2020-05-24 18:09:37 +0200

    Resolve #3

--- telegram.py
+++ telegram.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ####
-#### Usage: python telegram.py <okex-eliteScale.json> <telegram_bot_token>
+#### Usage: python telegram.py <timestamp> <telegram_bot_token> <output_dir>
 ####
 
 import sys
@@ -20,12 +20,24 @@ def btc_value():
     response = requests.get("https://blockchain.info/ticker")
     return str(response.json()['USD']['last'])
 
-filename = str(sys.argv[1])
-bot_token = str(sys.argv[2])
-threshold = 0.50
+def getEliteScale(timestamp):
+    response = requests.get("https://www.okex.com/v2/futures/pc/public/eliteScale.do?symbol=f_usd_btc&type=0&t="+str(timestamp))
+    return response.json()
 
-with open(filename) as f:
-  data = json.load(f)
+## ARGS and CONSTANTS
+version = "0.2"
+timestamp = str(sys.argv[1])
+bot_token = str(sys.argv[2])
+output_dir = str(sys.argv[3])
+threshold = 0.50
+prefix = "eliteScale-"
+filename = prefix + timestamp + ".json"
+
+data = getEliteScale(timestamp)
+
+#backup the data
+with open(output_dir + "/" + filename, 'w') as f:
+    f.write(str(data))
 
 #filter
 buydata = data['data']['buydata']
@@ -34,4 +46,4 @@ buy_value = buydata[len(buydata) - 1]
 sell_value = selldata[len(selldata) - 1]
 
 if buy_value >= threshold or sell_value >= threshold:
-    telegram_bot_sendtext("<<<WARN SENTIMENT >>>\n" + str(buy_value) + "% LONG vs " + str(sell_value) + "% SELL \nBTC value is " + btc_value() + " $", bot_token)
+    telegram_bot_sendtext("<<< WARN SENTIMENT ver. "+ version + " >>>\n" + str(buy_value) + "% LONG vs " + str(sell_value) + "% SELL \nBTC value is " + btc_value() + " $", bot_token)
